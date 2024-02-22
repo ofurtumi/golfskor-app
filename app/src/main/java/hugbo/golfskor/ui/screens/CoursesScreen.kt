03d294b313fbc623137e80ee540c0a085f249260
Þorvaldur Tumi <tumi@juni.is>
2024-02-22 12:25:47 +0000

    update courseScreen implementation

--- app/src/main/java/hugbo/golfskor/ui/screens/CoursesScreen.kt
+++ app/src/main/java/hugbo/golfskor/ui/screens/CoursesScreen.kt
@@ -1,40 +1,44 @@
 package hugbo.golfskor.ui.screens
 
-import android.content.res.Configuration
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.defaultMinSize
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
-import androidx.compose.material3.Divider
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-import androidx.navigation.NavHostController
-import hugbo.golfskor.entities.Round
-import hugbo.golfskor.ui.GolfRoundList
-import hugbo.golfskor.ui.theme.GolfskorTheme
+import androidx.lifecycle.viewmodel.compose.viewModel
+import hugbo.golfskor.entities.ApiCourse
+import hugbo.golfskor.ui.GolfCourseList
+import hugbo.golfskor.ui.viewModels.CourseUiState
+import hugbo.golfskor.ui.viewModels.CourseViewModel
 
 @Composable
-fun CoursesScreen() {
-    Column (
-        modifier = Modifier
-            .fillMaxWidth()
-            .fillMaxSize()
-    ) {
-        GolfRoundList()
+fun CoursesScreen(
+    courseViewModel: CourseViewModel = viewModel()
+) {
+    val courseUiState = courseViewModel.courseUiState
+    Column {
+        Button(onClick = { courseViewModel.refresh() }) {
+            Text(text = "Sækja aftur")
+        }
+        when (courseUiState) {
+            is CourseUiState.Loading -> LoadingScreen()
+            is CourseUiState.Success -> ResultScreen(courseUiState.courses)
+            is CourseUiState.Error -> ErrorScreen(courseUiState.message)
+        }
     }
 }
 
+@Composable
+fun ErrorScreen(message: String) {
+    Text(text = "Error: $message", color = MaterialTheme.colorScheme.error)
+}
+
+@Composable
+fun LoadingScreen() {
+    Text(text = "Loading")
+}
+
+@Composable
+fun ResultScreen(courses: List<ApiCourse>) {
+    GolfCourseList(courses)
+}
