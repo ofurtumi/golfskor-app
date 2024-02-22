8c9e65c985b0be591190d389f1e58c9480d156aa
Þorvaldur Tumi <tumi@juni.is>
2024-02-22 15:11:52 +0000

    update networkservice for logging

--- app/src/main/java/hugbo/golfskor/network/NetworkService.kt
+++ app/src/main/java/hugbo/golfskor/network/NetworkService.kt
@@ -3,18 +3,34 @@ package hugbo.golfskor.network
 import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
 import hugbo.golfskor.entities.ApiAuth
 import hugbo.golfskor.entities.ApiCourse
+import hugbo.golfskor.entities.ApiUserInfo
 import kotlinx.serialization.json.Json
 import okhttp3.MediaType.Companion.toMediaType
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.http.GET
+import retrofit2.http.Header
 import retrofit2.http.POST
 import retrofit2.http.Query
 
 private const val BASE_URL = "https://golfskor.onrender.com"
+
+val interceptor: HttpLoggingInterceptor = HttpLoggingInterceptor().apply {
+    level = HttpLoggingInterceptor.Level.HEADERS
+}
+
+val client: OkHttpClient = OkHttpClient.Builder().apply {
+    addInterceptor(interceptor)
+}.build()
+
+
 private val retrofit = Retrofit.Builder()
     .addConverterFactory(Json.asConverterFactory("application/json".toMediaType()))
     .baseUrl(BASE_URL)
+    .client(client)
     .build()
+
 
 interface NetworkService {
     @GET("api/courses")
@@ -31,6 +47,12 @@ interface NetworkService {
         @Query("username") username: String,
         @Query("password") password: String
     )
+
+    @GET("api/user/info")
+    suspend fun getUserRounds(
+        @Query("username") username: String,
+        @Header("Authorization") authToken: String
+    ): ApiUserInfo
 }
 
 object GolfSkorApi {
