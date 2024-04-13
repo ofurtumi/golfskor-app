7a5db8e1fa0d407b12b52f52ce9551f887c38f9d
Gunnarbjo <gbt6@hi.is>
2024-04-13 13:49:22 +0000

    Added doc to NavViewModel

--- app/src/main/java/hugbo/golfskor/ui/viewModels/NavViewModel.kt
+++ app/src/main/java/hugbo/golfskor/ui/viewModels/NavViewModel.kt
@@ -15,6 +15,18 @@ class NavViewModel : ViewModel() {
     var navUiState: NavUiState by mutableStateOf(NavUiState("", 0, ""))
         private set
 
+    /**
+     * Updates the navigation state with the current user's information.
+     *
+     * This function is used to set the user's credentials and identifiers in the navigation state, encapsulated in a
+     * {@code NavUiState} object. This allows other components of the application to access user-specific information
+     * needed for personalized interactions and API calls. It directly updates the {@code navUiState} variable of the
+     * ViewModel.
+     *
+     * @param username The username of the user.
+     * @param userId The unique identifier for the user, typically used in database transactions.
+     * @param authToken The authentication token used for secure API calls, proving the user's identity and session validity.
+     */
     fun setUserInfo(username: String, userId: Int, authToken: String) {
         navUiState = NavUiState(username, userId, authToken)
     }
