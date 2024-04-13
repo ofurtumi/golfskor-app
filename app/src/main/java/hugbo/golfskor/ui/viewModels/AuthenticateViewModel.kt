4d207a6816a3495e00fc8c6b0fb9abec893554b0
Gunnarbjo <gbt6@hi.is>
2024-04-13 13:43:02 +0000

    Added doc to AuthenticationViewModel

--- app/src/main/java/hugbo/golfskor/ui/viewModels/AuthenticateViewModel.kt
+++ app/src/main/java/hugbo/golfskor/ui/viewModels/AuthenticateViewModel.kt
@@ -26,6 +26,27 @@ class AuthenticateViewModel : ViewModel() {
     var authUiState: AuthUiState by mutableStateOf(AuthUiState.Starting)
         private set
 
+    /**
+     * Initialization block for the ViewModel handling authentication.
+     *
+     * This block is executed when an instance of the ViewModel is created. It initiates an asynchronous operation
+     * to fetch stored user credentials (username and authentication token) and determines the initial state of the
+     * authentication UI based on these credentials.
+     *
+     * The process includes:
+     * - Retrieving user information from a local data store.
+     * - Checking if the stored username and token are not empty. If valid credentials exist, it proceeds to refresh
+     *   the token by making an API call.
+     * - On successful token refresh, the authentication state is set to {@code AuthUiState.Success}, which includes
+     *   the new authentication details received from the server.
+     * - If the token refresh fails due to any exception (e.g., network errors, invalid token), the state is set to
+     *   {@code AuthUiState.ExpiredToken}, indicating that the existing token is no longer valid.
+     * - If no valid credentials are stored (i.e., username or token is empty), the state is set to {@code AuthUiState.LoginForm},
+     *   prompting the user to log in.
+     *
+     * The use of {@code viewModelScope} ensures that the coroutine launched for this process is automatically canceled
+     * if the ViewModel is cleared, preventing memory leaks and ensuring that the ViewModel's lifecycle is properly managed.
+     */
     init {
         viewModelScope.launch {
             val (username, authToken) = UserInfoDataStoreService.getUserInfo()
@@ -46,6 +67,23 @@ class AuthenticateViewModel : ViewModel() {
         }
     }
 
+    /**
+     * Attempts to authenticate a user with the provided username and password.
+     *
+     * This function is called to verify user credentials against a server using an API call. It sets the authentication
+     * state to {@code AuthUiState.LoginLoading} to indicate that a login attempt is in progress. A coroutine is launched
+     * within the {@code viewModelScope} to perform the asynchronous login request.
+     *
+     * The function executes the following steps:
+     * - Makes an API call to login with the specified username and password.
+     * - On successful authentication, saves the received authentication token using {@code UserInfoDataStoreService} and updates
+     *   the authentication state to {@code AuthUiState.Success}, which includes the authentication result.
+     * - If the login attempt fails due to any exception (e.g., network issues, incorrect credentials), updates the authentication
+     *   state to {@code AuthUiState.Error}, specifically indicating it as a login error.
+     *
+     * @param username The username provided by the user.
+     * @param password The password provided by the user.
+     */
     fun checkCredentials(username: String, password: String) {
         authUiState = AuthUiState.LoginLoading
         viewModelScope.launch {
@@ -59,6 +97,23 @@ class AuthenticateViewModel : ViewModel() {
         }
     }
 
+    /**
+     * Registers a new user with the specified username and password.
+     *
+     * This function initiates the user registration process by first setting the authentication state to
+     * {@code AuthUiState.SignupLoading} to indicate that a registration attempt is underway. It then launches
+     * a coroutine within the {@code viewModelScope} to perform the asynchronous registration request.
+     *
+     * The function performs the following operations:
+     * - Makes an API call to register the user with the provided username and password.
+     * - On successful registration, updates the authentication state to {@code AuthUiState.Registered}, signaling
+     *   that the user has been successfully registered.
+     * - If the registration attempt fails due to any exception (e.g., network issues, username already taken),
+     *   updates the authentication state to {@code AuthUiState.Error}, specifically indicating it as a registration error.
+     *
+     * @param username The username to be registered. Must be unique across the platform.
+     * @param password The password for the new user account.
+     */
     fun registerUser(username: String, password: String) {
         authUiState = AuthUiState.SignupLoading
         viewModelScope.launch {
